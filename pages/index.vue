4c4d80c8012f062c75d0aad196764679de511d25
Apfirebolt <aspper20@gmail.com>
2024-12-30 02:02:23 +0530

    some changes in the index page for deployment

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,20 @@
         <li class="mb-2">Filter Amiibo by series, game, and more</li>
       </ul>
       <p>Explore the world of Amiibo and discover your favorite characters!</p>
-      <ClientOnlyTest />
+
+      <p>
+        This application is built using Nuxt.js, Tailwind CSS, and the Amiibo API. It displays information related to characters, series, and games from the Amiibo database.
+      </p>
+      <!-- <ClientOnlyTest /> -->
+      <!-- <div class="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          @click="openModal"
+          class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+        >
+          Open dialog
+        </button>
+      </div> -->
       <Loader v-if="isLoading" />
 
       <div>
@@ -53,6 +66,64 @@
             <li v-if="item.release.na">NA: {{ item.release.na }}</li>
           </ul>
         </div>
+        <!-- <TransitionRoot appear :show="isOpen" as="template">
+          <Dialog as="div" @close="closeModal" class="relative z-10">
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0"
+              enter-to="opacity-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100"
+              leave-to="opacity-0"
+            >
+              <div class="fixed inset-0 bg-black/25" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto">
+              <div
+                class="flex min-h-full items-center justify-center p-4 text-center"
+              >
+                <TransitionChild
+                  as="template"
+                  enter="duration-300 ease-out"
+                  enter-from="opacity-0 scale-95"
+                  enter-to="opacity-100 scale-100"
+                  leave="duration-200 ease-in"
+                  leave-from="opacity-100 scale-100"
+                  leave-to="opacity-0 scale-95"
+                >
+                  <DialogPanel
+                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                  >
+                    <DialogTitle
+                      as="h3"
+                      class="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Payment successful
+                    </DialogTitle>
+                    <div class="mt-2">
+                      <p class="text-sm text-gray-500">
+                        Your payment has been successfully submitted. We’ve sent
+                        you an email with all of the details of your order.
+                      </p>
+                    </div>
+
+                    <div class="mt-4">
+                      <button
+                        type="button"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        @click="closeModal"
+                      >
+                        Got it, thanks!
+                      </button>
+                    </div>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
+            </div>
+          </Dialog>
+        </TransitionRoot> -->
       </div>
     </div>
   </NuxtLayout>
@@ -60,6 +131,13 @@
 <script setup>
 import axios from "axios";
 import { computed, onMounted, ref, watch } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
 
 definePageMeta({
   layout: false,
@@ -74,6 +152,14 @@ definePageMeta({
 
 const amiiboStore = useAmiibo();
 const searchText = ref("Mario");
+const isOpen = ref(true);
+
+function closeModal() {
+  isOpen.value = false;
+}
+function openModal() {
+  isOpen.value = true;
+}
 
 const itemList = computed(() => amiiboStore.getItemList);
 const isLoading = computed(() => amiiboStore.isLoading);
